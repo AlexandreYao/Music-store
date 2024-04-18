ac18b6812e8d9a1b0de6a2e030a783166df60cc9
Alexandre <ykoffialexandre@gmail.com>
2024-04-18 13:01:39 +0200

    continue work

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -76,47 +76,90 @@ LIMIT 1;
 -- 1. Write query to return the email, first name, last name of all Rock Music
 -- listeners. Return your list ordered alphabetically by email
 SELECT DISTINCT
-    email,
-    first_name,
-    last_name
+    customer.email,
+    customer.first_name,
+    customer.last_name
 FROM
     customer
 JOIN invoice ON customer.customer_id = invoice.customer_id
 JOIN invoice_line ON invoice.invoice_id = invoice_line.invoice_id
+JOIN track ON invoice_line.track_id = track.track_id
+JOIN genre ON track.genre_id = genre.genre_id
 WHERE
-    track_id IN(
-    SELECT
-        track_id
-    FROM
-        track
-    JOIN genre ON track.genre_id = genre.genre_id
-    WHERE
-        genre.name LIKE 'Rock'
-)
+    genre.name LIKE 'Rock%'
 ORDER BY
-    email;
+    customer.email;
 
 -- 2. Let's invite the artists who have written the most rock music in our dataset. Write a
 -- query that returns the Artist name and total track count of the top 10 rock bands
 SELECT
-    a.name,
-    COUNT(a.track_id) total_track_id
+    artist.name,
+    COUNT(track.track_id) total_track_id
 FROM
-    (
-    SELECT
-        track.track_id,
-        artist.name
-    FROM
-        track
-    JOIN genre ON track.genre_id = genre.genre_id
-    JOIN album2 ON album2.album_id = track.album_id
-    JOIN artist ON artist.artist_id = album2.artist_id
-    WHERE
-        genre.name LIKE '%Rock%'
-) a
+    track
+JOIN genre ON track.genre_id = genre.genre_id
+JOIN album2 ON album2.album_id = track.album_id
+JOIN artist ON artist.artist_id = album2.artist_id
+WHERE
+    genre.name LIKE 'Rock%'
 GROUP BY
-    a.name
+    artist.name
 ORDER BY
     total_track_id
 DESC
 LIMIT 10;
+
+-- 3. Return all the track names that have a song length longer than the average song length.
+-- Return the Name and Milliseconds for each track. Order by the song length with the
+-- longest songs listed first
+-- method 1
+SELECT
+    track.name,
+    track.milliseconds
+FROM
+    track
+JOIN(
+    SELECT
+        AVG(milliseconds) avg_milliseconds
+    FROM
+        track
+) avg_track
+WHERE
+    track.milliseconds >= avg_track.avg_milliseconds
+ORDER BY
+    track.milliseconds
+DESC;
+-- method 2
+SELECT
+    track.name,
+    track.milliseconds
+FROM
+    track
+WHERE
+    track.milliseconds >(
+    SELECT
+        AVG(track.milliseconds) avg_track_length
+    FROM
+        track
+)
+ORDER BY
+    track.milliseconds
+DESC;
+
+
+-- 1. Find how much amount spent by each customer on artists? Write a query to return
+-- customer name, artist name and total spent
+SELECT
+    customer.last_name,
+    artist.name,
+    SUM(invoice.total) total_spent
+FROM
+    invoice
+JOIN customer ON customer.customer_id = invoice.customer_id
+JOIN invoice_line ON invoice.invoice_id = invoice_line.invoice_id
+JOIN track ON invoice_line.track_id = track.track_id
+JOIN album2 ON album2.album_id = track.album_id
+JOIN artist ON album2.artist_id = artist.artist_id
+GROUP BY
+    customer.last_name,
+    artist.name;
