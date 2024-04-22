abb68c04fc53c41af083b80d6800b378fb3fb347
Alexandre <ykoffialexandre@gmail.com>
2024-04-22 10:35:57 +0200

    update of queries

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
--- 1. Qui est l'employé le plus ancien (niveau le plus élevé) en fonction du titre de poste ?
+-- 1. Who is the oldest employee (highest level) based on job title?
 SELECT
     title,
     last_name,
@@ -10,7 +10,7 @@ ORDER BY
 DESC
 LIMIT 1;
 
--- 2. Quels pays ont le plus grand nombre de factures ?
+-- 2. Which countries have the highest number of invoices?
 SELECT
     billing_country,
     COUNT(*) nb_invoices
@@ -22,7 +22,7 @@ ORDER BY
     nb_invoices
 DESC;
 
--- 3. Quelles sont les 3 valeurs les plus élevées du total des factures ?
+-- 3. What are the top 3 highest values of total invoices?
 SELECT DISTINCT
     total
 FROM
@@ -32,11 +32,10 @@ ORDER BY
 DESC
 LIMIT 3;
 
--- 4. Quelle ville a les meilleurs clients ?
--- Nous aimerions organiser un festival de musique promotionnel dans la ville où 
--- nous avons généré le plus de revenus. 
--- Écrivez une requête qui renvoie une seule ville ayant la somme la plus élevée de tous les totaux des factures. 
--- Renvoyez à la fois le nom de la ville et la somme de tous les totaux des factures.
+-- 4. Which city has the best customers?
+-- We would like to organize a promotional music festival in the city where we generated the most revenue. 
+-- Write a query that returns a single city having the highest sum of all invoice totals. 
+-- Return both the city name and the sum of all invoice totals.
 SELECT
     billing_city,
     SUM(total) invoice_total
@@ -49,9 +48,9 @@ ORDER BY
 DESC
 LIMIT 1;
 
--- 5. Qui est le meilleur client ? 
--- Le client qui a dépensé le plus d'argent sera déclaré meilleur client. 
--- Écrivez une requête qui renvoie la personne qui a dépensé le plus d'argent.
+-- 5. Who is the top customer?
+-- The customer who spent the most money will be declared the top customer. 
+-- Write a query that returns the person who spent the most money.
 SELECT
     c.customer_id,
     SUM(total) AS total_spent,
@@ -59,7 +58,7 @@ SELECT
         MIN(c.first_name),
         ' ',
         MIN(c.last_name)
-    ) AS customer_name
+    ) AS customer
 FROM
     customers c
 JOIN invoices i ON
@@ -72,94 +71,160 @@ DESC
 LIMIT 1;
 
 
--- Question Set 2 – Moderate
--- 1. Write query to return the email, first name, last name of all Rock Music
--- listeners. Return your list ordered alphabetically by email
+-- 6. Write a query to return the email, first name, last name of all rock music listeners. 
+-- Return your list ordered alphabetically by email.
 SELECT DISTINCT
-    customer.email,
-    customer.first_name,
-    customer.last_name
+    c.email,
+    c.first_name,
+    c.last_name
 FROM
-    customer
-JOIN invoice ON customer.customer_id = invoice.customer_id
-JOIN invoice_line ON invoice.invoice_id = invoice_line.invoice_id
-JOIN track ON invoice_line.track_id = track.track_id
-JOIN genre ON track.genre_id = genre.genre_id
+    customers c
+JOIN invoices i ON
+    c.customer_id = i.customer_id
+JOIN invoice_lines il ON
+    i.invoice_id = il.invoice_id
+JOIN tracks t ON
+    il.track_id = t.track_id
+JOIN genres g ON
+    t.genre_id = g.genre_id
 WHERE
-    genre.name LIKE 'Rock%'
+    g.name LIKE 'Rock%'
 ORDER BY
-    customer.email;
+    c.email;
 
--- 2. Let's invite the artists who have written the most rock music in our dataset. Write a
--- query that returns the Artist name and total track count of the top 10 rock bands
+-- 7. Let's invite the artists who have written the most rock music in our dataset. 
+-- Write a query that returns the artist name and total track count of the top 10 rock artists.
 SELECT
-    artist.name,
-    COUNT(track.track_id) total_track_id
+    artists.name,
+    COUNT(tracks.track_id) total_tracks
 FROM
-    track
-JOIN genre ON track.genre_id = genre.genre_id
-JOIN album2 ON album2.album_id = track.album_id
-JOIN artist ON artist.artist_id = album2.artist_id
+    tracks
+JOIN genres ON tracks.genre_id = genres.genre_id
+JOIN albums ON albums.album_id = tracks.album_id
+JOIN artists ON artists.artist_id = albums.artist_id
 WHERE
-    genre.name LIKE 'Rock%'
+    genres.name LIKE 'Rock%'
 GROUP BY
-    artist.name
+    artists.name
 ORDER BY
-    total_track_id
+    total_tracks
 DESC
 LIMIT 10;
 
--- 3. Return all the track names that have a song length longer than the average song length.
--- Return the Name and Milliseconds for each track. Order by the song length with the
--- longest songs listed first
+-- 8. Return all the track names that have a song length longer than the average song length. 
+-- Return the name and milliseconds for each track. 
+-- Order by the song length with the longest songs listed first.
 -- method 1
 SELECT
-    track.name,
-    track.milliseconds
+    tracks.name,
+    tracks.milliseconds
 FROM
-    track
+    tracks
 JOIN(
     SELECT
         AVG(milliseconds) avg_milliseconds
     FROM
-        track
+        tracks
 ) avg_track
 WHERE
-    track.milliseconds >= avg_track.avg_milliseconds
+    tracks.milliseconds >= avg_track.avg_milliseconds
 ORDER BY
-    track.milliseconds
+    tracks.milliseconds
 DESC;
 -- method 2
 SELECT
-    track.name,
-    track.milliseconds
+    tracks.name,
+    tracks.milliseconds
 FROM
-    track
+    tracks
 WHERE
-    track.milliseconds >(
+    tracks.milliseconds >(
     SELECT
-        AVG(track.milliseconds) avg_track_length
+        AVG(tracks.milliseconds) avg_track_length
     FROM
-        track
+        tracks
 )
 ORDER BY
-    track.milliseconds
+    tracks.milliseconds
 DESC;
 
-
--- 1. Find how much amount spent by each customer on artists? Write a query to return
--- customer name, artist name and total spent
+-- 9. Find how much amount spent by each customer on artists? 
+-- Write a query to return customer name, artist name, and total spent.
 SELECT
-    customer.last_name,
-    artist.name,
-    SUM(invoice.total) total_spent
+    CONCAT(
+        MIN(c.first_name),
+        ' ',
+        MIN(c.last_name)
+    ) AS customer_name,
+    MIN(ar.name) AS artist_name,
+    SUM(il.unit_price * il.quantity) AS total_spent
 FROM
-    invoice
-JOIN customer ON customer.customer_id = invoice.customer_id
-JOIN invoice_line ON invoice.invoice_id = invoice_line.invoice_id
-JOIN track ON invoice_line.track_id = track.track_id
-JOIN album2 ON album2.album_id = track.album_id
-JOIN artist ON album2.artist_id = artist.artist_id
+    invoice_lines il
+JOIN invoices i ON
+    i.invoice_id = il.invoice_id
+JOIN customers c ON
+    c.customer_id = i.customer_id
+JOIN tracks t ON
+    il.track_id = t.track_id
+JOIN albums al ON
+    al.album_id = t.album_id
+JOIN artists ar ON
+    al.artist_id = ar.artist_id
 GROUP BY
-    customer.last_name,
-    artist.name;
+    c.customer_id,
+    ar.artist_id;
+
+-- 10. We want to find out the most popular music genre for each country. 
+-- We define the most popular genre as the genre with the highest number of track purchases.
+-- Write a query that returns each country along with the top genre. 
+-- For countries where the maximum number of purchases is shared, return all genres.
+SELECT
+    a.country,
+    genre_name,
+    nb_purchases
+FROM
+    (
+    SELECT
+        i.billing_country AS country,
+        MIN(g.name) AS genre_name,
+        COUNT(t.track_id) AS nb_purchases
+    FROM
+        invoice_lines il
+    JOIN invoices i ON
+        i.invoice_id = il.invoice_id
+    JOIN tracks t ON
+        t.track_id = il.track_id
+    JOIN genres g ON
+        g.genre_id = t.genre_id
+    GROUP BY
+        i.billing_country,
+        g.genre_id
+) a
+JOIN(
+    SELECT
+        sub.country AS country,
+        MAX(nb_purchases) AS max_nb_purchases
+    FROM
+        (
+        SELECT
+            i.billing_country AS country,
+            COUNT(t.track_id) AS nb_purchases
+        FROM
+            invoice_lines il
+        JOIN invoices i ON
+            i.invoice_id = il.invoice_id
+        JOIN tracks t ON
+            t.track_id = il.track_id
+        GROUP BY
+            i.billing_country,
+            t.genre_id
+    ) sub
+GROUP BY
+    country
+) b
+ON
+    a.country = b.country AND a.nb_purchases = b.max_nb_purchases
+ORDER BY
+    a.country,
+    nb_purchases
+DESC;
